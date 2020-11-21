344c0bbe1c7a0f30e07df6a810f17c4f4e7132e1
Marius Menault <mouarius@gmail.com>
2020-11-21 19:34:48 +0100

    Button increment to change the state

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,62 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => (
-    <h1>{props.course.name}</h1>
-);
+const Display = ({counter}) => (<div>{counter}</div>)
 
-const Content = (props) => {
-  var parts = [];
-  for (let i=0 ; i < props.course.parts.length ; i++){
-    parts.push(props.course.parts[i]);
-  }
-  return(
-    <div>
-      <Part part= {parts[0]}/>
-      <Part part= {parts[1]}/>
-      <Part part= {parts[2]}/>
-    </div>
-  )};
+const Button = ({handleClick, text}) => (<button onClick = {handleClick}> {text} </button>)
 
-const Part = (props) => {
-  return(<p>{props.part.name} {props.part.exercises}</p>);
-}
 
-const Total = (props) => {
-  let total = 0;
-  for(let i = 0 ; i<props.course.parts.length ; i++){
-      total = total + props.course.parts[i].exercises;
-  }
-  return(
-    <p>Number of exercises {total}</p>
-  )};
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [ counter, setCounter ] = useState(0)
+
+  const incrementCounter = () => {
+    return setCounter(counter + 1);
+  }
+
+  const decrementCounter = () => {
+    return setCounter(counter - 1)
+  }
+
+  const resetCounter = () => {
+    return setCounter(0)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Display counter={counter}/>
+      <Button handleClick={decrementCounter} text="MOINS"/>
+      <Button handleClick={resetCounter} text="RESET"/>
+      <Button handleClick={incrementCounter} text="PLUS"/>
     </div>
-  )
+  );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
