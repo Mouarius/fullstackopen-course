b354f927798e2b7b459f4c4b28266a61b4b9b236
Marius Menault <mouarius@gmail.com>
2020-11-21 20:04:05 +0100

    Adding a test for the stylesheets

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
+import './assets/bootstrap/css/bootstrap.min.css'
+import './assets/css/styles.css'
 
-const Display = ({counter}) => (<div>{counter}</div>)
 
-const Button = ({handleClick, text}) => (<button onClick = {handleClick}> {text} </button>)
-
-
+const Header = () => (<h1 className="display-3 text-center">Heading</h1>)
+const Display = () => (
+<div className="container text-center">
+  <p>COMPTEUR</p>
+</div>
+)
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const incrementCounter = () => {
-    return setCounter(counter + 1);
-  }
-
-  const decrementCounter = () => {
-    return setCounter(counter - 1)
-  }
-
-  const resetCounter = () => {
-    return setCounter(0)
-  }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
-    <div>
-      <Display counter={counter}/>
-      <Button handleClick={decrementCounter} text="MOINS"/>
-      <Button handleClick={resetCounter} text="RESET"/>
-      <Button handleClick={incrementCounter} text="PLUS"/>
+    <div className="container text-center">
+      <Header/>
+      {left}
+      <button className="btn btn-primary" onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button className="btn btn-primary" onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
-  );
+  )
 }
 ReactDOM.render(
   <App />,
