4e5712573e08143d1b6ed7b53224562486412d8f
mouarius <mouarius@gmail.com>
2020-11-22 23:42:18 +0100

    Exercise 1.6 step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -22,10 +22,42 @@ const App = () => {
   const [numberNeutral, setNumberNeutral] = useState(0);
   const [numberBad, setNumberBad] = useState(0);
 
-  const addGood = () => (setNumberGood(numberGood + 1))
-  const addNeutral = () => (setNumberNeutral(numberNeutral + 1))
-  const addBad = () => (setNumberBad(numberBad + 1))
+  const [numberAverage, setNumberAverage] = useState(0);
+  const [numberPositive, setNumberPositive] = useState(0);
 
+  const all = numberBad + numberNeutral + numberGood;
+  
+  //A method to calculate the average of the rates given in input, be careful, the current state variable when we calculate is the previous one, we will need to add also 1 on the state for the update
+  const calculateAverage = (good,neutral,bad) => { 
+    if (good + neutral + bad !== 0){
+      return ((good - bad)/(good+bad+neutral))
+    }
+    return 0
+  }
+
+  const calculatePositive = (good, neutral, bad) => {
+    if (good + neutral + bad !== 0){
+      return(good/(good+neutral+bad)*100)
+    }
+    return 0
+  }
+
+  const addGood = () => {
+    setNumberGood(numberGood + 1)
+    setNumberAverage(calculateAverage(numberGood +1, numberNeutral, numberBad)) // We need to add 1 to numberGood because on calling setNumberGood(numberGood+1) the re-render is only cued and so when we call setNumberAverage, the numberGood value hasn't been updated
+    setNumberPositive(calculatePositive(numberGood+1, numberNeutral, numberBad))
+  }
+  const addNeutral = () => {
+    setNumberNeutral(numberNeutral + 1)
+    setNumberAverage(calculateAverage(numberGood, numberNeutral+1, numberBad))
+    setNumberPositive(calculatePositive(numberGood, numberNeutral+1, numberBad))
+
+  }
+  const addBad = () => {
+    setNumberBad(numberBad + 1)
+    setNumberAverage(calculateAverage(numberGood, numberNeutral, numberBad+1))
+    setNumberPositive(calculatePositive(numberGood, numberNeutral, numberBad+1))
+  }
 
   return(
     <div>
@@ -35,6 +67,9 @@ const App = () => {
       <Button clickHandler={addBad} text={"bad"}/>
 
       <Stats numbers = {[numberGood, numberNeutral, numberBad]}/>
+      <p>all : {all}</p>
+      <p>average : {numberAverage}</p>
+      <p>positive : {numberPositive} %</p>
     </div>
   )
 
