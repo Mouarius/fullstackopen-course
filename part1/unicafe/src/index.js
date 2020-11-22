a14062aafa2666e7e89f0d08bf60fe838a630c8d
mouarius <mouarius@gmail.com>
2020-11-22 23:56:59 +0100

    Exercise 1.8 step3 + percentage positive

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,14 +1,31 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Stats = ({numbers}) => {
+const Statistics = ({numbers}) => {
   const [numberGood, numberNeutral, numberBad] = numbers
+
+  //COMPUTED VALUES
+  const all = numberBad + numberNeutral + numberGood;
+
+  var average = 0
+  if (all !== 0){
+    average = (numberGood - numberBad)/all
+  }
+
+  var positive = 0
+  if (all !== 0){
+    positive = (numberGood)/all*100
+  }
+
   return(
     <div>
       <h1>Statistics</h1>
       <p>good : {numberGood}</p>
-        <p>neutral : {numberNeutral}</p>
-        <p>bad : {numberBad}</p>
+      <p>neutral : {numberNeutral}</p>
+      <p>bad : {numberBad}</p>
+      <p>all : {all}</p>
+      <p>average : {average}</p>
+      <p>positive : {positive} %</p>
     </div>
   )
 }
@@ -23,18 +40,7 @@ const App = () => {
   const [numberNeutral, setNumberNeutral] = useState(0);
   const [numberBad, setNumberBad] = useState(0);
 
-  //STATISTICS VALUES
-  const all = numberBad + numberNeutral + numberGood;
-
-  var average = 0
-  if (all !== 0){
-    average = (numberGood - numberBad)/all
-  }
-
-  var positive = 0
-  if (all !== 0){
-    positive = (numberGood)/all
-  }
+  
   //Note : the computed statistic values don't have to be states, they are direcly calculated at each re-render, we don't have to create a new complex state just to re render it.
 
   const addGood = () => {
@@ -54,11 +60,7 @@ const App = () => {
       <Button clickHandler={addNeutral} text={"neutral"}/>
       <Button clickHandler={addBad} text={"bad"}/>
 
-      <Stats numbers = {[numberGood, numberNeutral, numberBad]}/>
-      <p>all : {all}</p>
-      <p>average : {average}</p>
-      <p>positive : {positive} %</p>
-
+      <Statistics numbers = {[numberGood, numberNeutral, numberBad]}/>
     </div>
   )
 
