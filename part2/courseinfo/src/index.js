10a56b2ea54d1970bf2fdf7387b57f23b49867b3
mouarius <mouarius@gmail.com>
2020-11-24 00:50:09 +0100

    2.1-part6

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,62 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => (
-    <h1>{props.course.name}</h1>
+const Header = ({course}) => (
+    <h1>{course.name}</h1>
 );
 
-const Content = (props) => {
-  var parts = [];
-  for (let i=0 ; i < props.course.parts.length ; i++){
-    parts.push(props.course.parts[i]);
-  }
+const Content = ({course}) => {
+  const parts = [...course.parts]
   return(
     <div>
-      <Part part= {parts[0]}/>
-      <Part part= {parts[1]}/>
-      <Part part= {parts[2]}/>
+      {parts.map((part) => (
+        <Part key={part.id} part={part}/>
+      ))}
     </div>
   )};
 
-const Part = (props) => {
-  return(<p>{props.part.name} {props.part.exercises}</p>);
+const Part = ({part}) => {
+  return(<p>{part.name} {part.exercises}</p>);
 }
 
-const Total = (props) => {
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+    </div>
+  )
+}
+
+/* const Total = (props) => {
   let total = 0;
   for(let i = 0 ; i<props.course.parts.length ; i++){
       total = total + props.course.parts[i].exercises;
   }
   return(
     <p>Number of exercises {total}</p>
-  )};
+  )}; */
+
+
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
