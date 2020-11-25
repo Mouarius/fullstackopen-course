35f50e48f27ea51fa7b74307e35064f4ce7166e8
mouarius <mouarius@gmail.com>
2020-11-25 19:17:11 +0100

    Exercise 2.9 step4 - filter case insensitive

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,42 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  //------    COMPONENT STATES    ------//
   const [ persons, setPersons ] = useState([
-    {
-        name: 'Arto Hellas',
-        phone: '0612345678'
-    }
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]) 
-  console.log(persons);
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ filterValue, setFilterValue ] = useState('')
 
+  //------    PERSONS FILTERING PROCESS    ------//
+  
+  const personsToShow = [] //Create an empty array to contain the objects of persons to show, we need to declare it empty at each new state render to ensure that we only show the current filtervalue
+
+  for(let i = 0 ; i < persons.length ; i++){
+
+    //LOG DEVELOPPEMENT TESTS
+    /* console.log(`persons[${i}] :>> `, persons[i].name.toLowerCase());
+    console.log('includes a >>', persons[i].name.toLowerCase().includes('a')); */
+
+    if (persons[i].name.toLowerCase().includes(filterValue.toLowerCase())){ //Compares the values of lower case name property of deach person in persons, with the actual filter value (lower case as well)
+      personsToShow.push(persons[i])//If there is a match, we push the person to the persons to show array
+      console.log('Render the person :', persons[i].name);
+    }
+  }
+  //const personsToShow = personsLowerCase.filter(person => person.name.includes(filterValue.toLowerCase()))
+  console.log('Persons to show -> ', personsToShow)
+
+
+  //------    EVENT HANDLERS    ------//
+
+  const handleFilterChange = (event) => {
+    console.log('Filter -> ', event.target.value)
+    setFilterValue(event.target.value)
+  }
 
   const handleNameChange = (event) => {
     console.log('Current input value : ', event.target.value);
@@ -21,8 +47,12 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  //------    ADD PERSON BUTTON HANDLER    ------//
+
   const addPerson = (event) => {
+
     event.preventDefault()
+
     if (persons.map(person => person.name).includes(newName)) {
         console.log(`The entered name ${newName} already exists.`);
         alert(`${newName} already exists !`)
@@ -31,18 +61,20 @@ const App = () => {
         setPersons(persons.concat({name: newName, phone: newPhone}))
     }
   }
-
+  //------    RETURN FIELD OF THE COMPONENT    ------//
   return (
+
     <div>
       <h2>Phonebook</h2>
+      Filter by name : <input onChange={handleFilterChange} type="text"/>
       <form onSubmit={addPerson}>
         <ul>
             <li>
-                <label for="name">Name :</label>
+                <label htmlFor="name">Name :</label>
                 <input onChange={handleNameChange} type='text' id='name' value={newName}/>
             </li>
             <li>
-                <label for="phone">Phone :</label>
+                <label htmlFor="phone">Phone :</label>
                 <input onChange={handlePhoneChange} type='phone' id='phone' value={newPhone}/>
             </li>
         </ul>
@@ -53,7 +85,7 @@ const App = () => {
       <h2>Numbers</h2>
       <table>
           <tbody>
-            {persons.map(person => 
+            {personsToShow.map(person => 
             <tr key={person.name}>
                 <td>{person.name}</td>
                 <td>{person.phone}</td>
@@ -63,6 +95,7 @@ const App = () => {
           
       </table>
     </div>
+
   )
 }
 
